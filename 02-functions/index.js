652586798f5dfd84992769537e5fd8108b55c9e0
guruzaran-dev <thavarathinamguruzaran@gmail.com>
2025-12-25 23:10:20 +0530

    Add functions basics

---
+++ 02-functions/index.js
@@ -0,0 +1,13 @@
+// JavaScript Functions
+
+function greet(name) {
+  return "Hello, " + name;
+}
+
+console.log(greet("Zaran"));
+
+const add = (a, b) => {
+  return a + b;
+};
+
+console.log(add(5, 3));
