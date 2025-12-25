25d118ce67695501f0232dab4253113b19917417
guruzaran-dev <thavarathinamguruzaran@gmail.com>
2025-12-25 23:07:49 +0530

    Add variables basics

---
+++ 01-variables/index.js
@@ -0,0 +1,12 @@
+// JavaScript Variables
+
+let name = "Zaran";
+const age = 22;
+let isDeveloper = true;
+
+console.log(name);
+console.log(age);
+console.log(isDeveloper);
+
+name = "Zaran Dev";
+console.log(name);
